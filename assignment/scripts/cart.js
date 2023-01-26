25965e5a4db63f284e2bcf943c1e9dba3d761cae
Erich Engler <erich.engler@hotmail.com>
2023-01-25 20:26:53 -0600

    finished required problems and 1st stretch question

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,53 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+const maxItems = 5;
+let basket = [];
+console.log( `basket is ${basket}` );
+
+function addItem( item ) {
+    console.log( isFull() );
+    if ( isFull() === true ){
+        isFull( basket );
+        basket.push( item );
+        return true;
+    }
+}
+
+addItem( 'chips' );
+addItem( 'bread' );
+addItem( 'milk' );
+addItem( 'cookies' );
+addItem( 'cheese' );
+addItem( 'popcorn' );
+console.log ( `basket is now ${basket}` );
+
+function listItems() {
+    for ( let grocery of basket ) {
+        console.log( grocery );
+    }
+}
+
+listItems();
+
+function empty() {
+    basket = [];
+}
+
+empty( basket );
+console.log( basket );
+
+// STRETCH PROBLEMS
+
+function isFull() {
+    if ( basket.length < maxItems ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function removeItem( item ) {
+    
+}
